fb077ec7d030a5d8d358df38d979e14bf435af79
Iman Reihanian <ireihani@uci.edu>
2023-08-08 15:15:44 -0700

    Addition Code

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@ function activate(context) {
   /**
    * This command prompts the user for their OpenAI key if it's not already stored,
    * gets the diff of the staged files, sends the diff to the OpenAI API as the prompt,
-   * and shows the response from the API.
+   * and opens the response from the API in a new markdown editor tab.
    */
   let disposable = vscode.commands.registerCommand("gitgpt", async () => {
     // Prompt the user to enter their OpenAI key if it's not already stored
@@ -46,36 +46,71 @@ function activate(context) {
         return;
       }
 
-      // Send the diff to the OpenAI API to generate a commit comment
-      try {
-        let response = await axios.post(
-          "https://api.openai.com/v1/engines/davinci-codex/completions",
-          {
-            prompt: `Generate a commit comment for the following changes:\n\n${diff}`,
-            max_tokens: 60,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${openAIKey}`,
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        // Show the generated commit comment
-        let commitComment = response.data.choices[0].text.trim();
-        vscode.window.showInformationMessage(commitComment);
-      } catch (err) {
-        vscode.window.showErrorMessage(
-          "Error generating commit comment: " + err
-        );
+      // Truncate the diff if it's too large for the OpenAI API
+      if (diff.length > 4096) {
+        diff = diff.substring(0, 4096);
       }
+
+      // Get the diff of the git repository
+      gitRepo.diff(["--staged"], async (err, diff) => {
+        if (err) {
+          vscode.window.showErrorMessage("Error getting git diff: " + err);
+          return;
+        }
+
+        // Send the diff to the OpenAI API to generate a commit comment
+        try {
+          let response = await axios.post(
+            "https://api.openai.com/v1/chat/completions",
+            {
+              model: "gpt-3.5-turbo-16k-0613",
+              messages: [
+                {
+                  role: "system",
+                  content:
+                    "You are a helpful assistant that generates commit messages based on code changes.",
+                },
+                {
+                  role: "user",
+                  content:
+                    "Here are the code changes in the following files: " + diff,
+                },
+                { role: "assistant", content: diff.replace(/\n/g, "\\n") },
+                {
+                  role: "user",
+                  content: "Generate a commit message for these changes.",
+                },
+              ],
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${openAIKey}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+
+          // Create a new text document with the generated commit comment
+          let commitComment = response.data.choices[0].message.content.trim();
+          let doc = await vscode.workspace.openTextDocument({
+            content: commitComment,
+            language: "markdown",
+          });
+
+          // Open the document in a new editor tab
+          vscode.window.showTextDocument(doc);
+        } catch (err) {
+          vscode.window.showErrorMessage(
+            "Error generating commit message: " + err
+          );
+        }
+      });
     });
   });
 
   context.subscriptions.push(disposable);
 
-  // Create a new status bar item that calls the command
+  // Create a new status bar item that calls the 'gitgpt' command
   let statusBarIcon = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left
   );
